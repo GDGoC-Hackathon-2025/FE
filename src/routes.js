6e93c4ab5de154620b1c67f8e29695c886a0b483
parknari02 <pnr021118@naver.com>
2025-01-19 01:21:51 +0900

    ✨ Feat: 소셜 로그인 페이지 구현

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import LoginPage from "./pages/login/LoginPage";
 import ComplainPage from "./pages/complain/Complain";
 import Header from "./components/Header";
@@ -7,19 +7,29 @@ import ComplainCreatePage from "./pages/complain-create/ComplainCreatePage";
 import FundingCreatePage from "./pages/funding-create/FundingCreatePage";
 
 const AppRouter = () => {
+  const location = useLocation();
+
   return (
-    <Router>
-      <Header />
+    <>
+      {location.pathname !== "/auth" && <Header />}
       <Routes>
         <Route path="/" element={<ComplainPage />} />
         <Route path="/complain/create" element={<ComplainCreatePage />} />
         <Route path="/auth" element={<LoginPage />} />
         <Route path="/funding" element={""} />
-        <Route path="/funding/create" element={<FundingCreatePage/>} />
+        <Route path="/funding/create" element={<FundingCreatePage />} />
         <Route path="/payment" element={""} />
       </Routes>
+    </>
+  );
+};
+
+const AppWrapper = () => {
+  return (
+    <Router>
+      <AppRouter />
     </Router>
   );
 };
 
-export default AppRouter;
+export default AppWrapper;
