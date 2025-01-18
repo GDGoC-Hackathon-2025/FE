357f1ed45d431765e0a74152f598d477e7c7844f
xoxun <00xxs2@naver.com>
2025-01-19 03:46:34 +0900

    🚑 Fix: 민원 상세 페이지 라우팅 오류

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import LoginPage from "./pages/login/LoginPage";
 import ComplainPage from "./pages/complain/Complain";
 import Header from "./components/Header";
 import ComplainCreatePage from "./pages/complain-create/ComplainCreatePage";
 import FundingCreatePage from "./pages/funding-create/FundingCreatePage";
 import FundingList from "./pages/funding/FundingList";
-import FundingDetail from "./pages/funding-detail/FundingDetail";
 import LoginInfoPage from "./pages/login-info/LoginInfoPage";
 import MyPage from "./pages/my/MyPage";
-
+import FundingDetail from "./pages/funding-detail/FundingDetail";
+import ComplainDetail from "./pages/complain-detail/ComplainDetail";
 
 const AppRouter = () => {
   const location = useLocation();
@@ -21,6 +26,7 @@ const AppRouter = () => {
       {!hideHeaderPaths.includes(location.pathname) && <Header />}
       <Routes>
         <Route path="/" element={<ComplainPage />} />
+        <Route path="/complain/:id" element={<ComplainDetail />} />
         <Route path="/complain/create" element={<ComplainCreatePage />} />
         <Route path="/auth" element={<LoginPage />} />
         <Route path="/auth/info" element={<LoginInfoPage />} />
@@ -28,7 +34,7 @@ const AppRouter = () => {
         <Route path="/funding/:id" element={<FundingDetail />} />
         <Route path="/funding/create" element={<FundingCreatePage />} />
         <Route path="/payment" element={""} />
-        <Route path="/my" element={<MyPage/>} />
+        <Route path="/my" element={<MyPage />} />
       </Routes>
     </>
   );
@@ -43,4 +49,3 @@ const AppWrapper = () => {
 };
 
 export default AppWrapper;
-
